67e0b9e697c85006f436f73ae697f6d8cf6f38af
Koffyy <69871603+Koffyy@users.noreply.github.com>
2020-10-28 22:55:41 -0300

    Update adventure.js

--- cmds/adventure.js
+++ cmds/adventure.js
@@ -2,6 +2,8 @@ const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
    message.channel.send('comming')
+   
+   message.delete()
 }
 
 module.exports.help = {
